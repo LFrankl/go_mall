e394657d2818e506ce61f274471ab3c54491ad9c
lfq <fuquanliang0@gmail.com>
2025-11-10 16:35:57 +0800

    完成简单的rabbitmq 生产者-消费者测试

--- consumer/main.go
+++ consumer/main.go
@@ -1,5 +1,59 @@
-package consumer
+package main
+
+import (
+	"baby/rabbitmq"
+	"fmt"
+	"github.com/rabbitmq/amqp091-go"
+)
+
+/*
+
+写一个处理所有业务
+复杂度上来了再解耦
+
+先全局初始化一个Client，各个模块都能使用
+
+消费者进程也要写
+DeclareOrderExchangeQueue()
+......等
+
+*/
+
+type Client struct {
+	conn    *amqp091.Connection
+	channel *amqp091.Channel
+}
+
+var cli *rabbitmq.Client
+
+func init() {
+	fmt.Println("connecting to rabbitmq")
+	var err error
+	cli, err = rabbitmq.NewClient("localhost:5672", "guest", "guest")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("connected ")
+	return
+
+}
 
 func main() {
+
+	err := cli.DeclareOrderExchangeQueue()
+	if err != nil {
+		panic(err)
+	}
+
+	msgs, err := cli.ConsumeOrder()
+	if err != nil {
+		panic(err)
+	}
+
+	for msg := range msgs {
+		// msg 是 amqp091.Delivery 类型
+		fmt.Printf("收到消息：路由键=%s，内容=%s\n", msg.RoutingKey, string(msg.Body))
+
+	}
 
 }
--- main.go
+++ main.go
@@ -2,19 +2,50 @@ package main
 
 import (
 	"baby/models"
+	"baby/rabbitmq"
 	"baby/routers"
+	"fmt"
 	"net/http"
 	"time"
 )
 
+var cli *rabbitmq.Client
+
 func init() {
+
+	fmt.Println("rabbitmq init")
+
 	err := models.Setup()
 	if err != nil {
 		panic(err)
 	}
+
+	cli, err = rabbitmq.NewClient("localhost:5672", "guest", "guest")
+	if err != nil {
+		panic(err)
+	}
+	err = cli.DeclareOrderExchangeQueue()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("rabbitmq init success")
 }
 
 func main() {
+
+	msg := models.LoginLogMessage{
+		OrderID:     999,
+		UserID:      998,
+		CommodityID: 3432,
+		Quantity:    9999,
+		Price:       9.9,
+	}
+
+	errr := cli.PublishOrder(msg)
+	if errr != nil {
+		panic(errr)
+	}
 
 	server := &http.Server{
 		Addr:         ":8080",
